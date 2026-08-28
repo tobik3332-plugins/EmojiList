f3b8da904031eac413945f6e7e0aecc9ee499586
tobik3332-plugins <faktnevim174@gmail.com>
2026-08-28 22:00:55 +0200

    Update EmojiList.java

--- src/main/java/cz/emojilist/EmojiList.java
+++ src/main/java/cz/emojilist/EmojiList.java
@@ -1,5 +1,6 @@
 package cz.emojilist;
 
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EmojiList extends JavaPlugin {
@@ -11,8 +12,11 @@ public final class EmojiList extends JavaPlugin {
         this.configManager = new ConfigManager(this);
         this.configManager.loadConfig();
 
-        if (getCommand("emojis") != null) {
-            getCommand("emojis").setExecutor(new EmojiCommand(this));
+        PluginCommand command = getCommand("emojis");
+        if (command != null) {
+            EmojiCommand emojiCommand = new EmojiCommand(this);
+            command.setExecutor(emojiCommand);
+            command.setTabCompleter(emojiCommand); // Registrace našeptávače
         }
 
         getLogger().info("Plugin EmojiList byl úspěšně aktivován!");
